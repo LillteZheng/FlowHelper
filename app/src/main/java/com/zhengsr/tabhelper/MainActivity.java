721c6e95f94db374054a986f349568bf720b5b1a
LillteZheng <845731923@qq.com>
2020-01-02 16:14:10 +0800

    首次添加： xml 中动态布局

---
+++ app/src/main/java/com/zhengsr/tabhelper/MainActivity.java
@@ -0,0 +1,22 @@
+package com.zhengsr.tabhelper;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.zhengsr.tabhelper.activity.TabActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, TabActivity.class));
+    }
+
+    public void tablayout(View view) {
+        startActivity(new Intent(this, TabActivity.class));
+    }
+}
---
+++ app/src/main/java/com/zhengsr/tabhelper/activity/TabActivity.java
@@ -0,0 +1,15 @@
+package com.zhengsr.tabhelper.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhengsr.tabhelper.R;
+
+public class TabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab);
+    }
+}
